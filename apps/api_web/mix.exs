a01cc66d58e38e6040d2b2714617846ea37e129a
a.radushev <a.radushev@starladder.tv>
2019-08-07 18:34:39 +0300

    guardian

--- apps/api_web/mix.exs
+++ apps/api_web/mix.exs
@@ -50,8 +50,7 @@ defmodule ApiWeb.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:poison, "~> 3.1"},
-#      {:guardian, git: "https://github.com/ueberauth/guardian.git"},
-      {:guardian, "~> 1.0"},
+      {:guardian, "~> 2.0"},
       {:ueberauth_facebook, "~> 0.8"},
       {:cors_plug, "~> 2.0"}
     ]
