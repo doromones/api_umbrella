0bf4647090f987e1ee357bb29f10c68217fdd396
a.radushev <a.radushev@starladder.tv>
2019-08-20 18:44:12 +0300

    tsx => jsx

---
+++ apps/core_web/assets/js/components/Index.jsx
@@ -0,0 +1,15 @@
+// assets/js/pages/index.tsx
+
+import * as React from 'react'
+import Main from '../components/Main'
+import {Component} from "react";
+
+class HomePage extends Component {
+    render() {
+        return (
+            <Main>HomePage</Main>
+        )
+    }
+}
+
+export default HomePage
