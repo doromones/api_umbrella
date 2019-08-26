5dd7bc5f4c9b9055b0d0fd4166a7578482fef58b
doromones <doromones@gmail.com>
2019-08-26 20:00:43 +0300

    root to component

--- apps/core_web/assets/js/Root.tsx
+++ apps/core_web/assets/js/Root.tsx
@@ -7,18 +7,20 @@ import CounterPage from './pages/counter';
 import FetchDataPage from './pages/fetch-data';
 import AuthPage from './pages/auth';
 
-const Root: React.FC = () => (
-    <>
-        <Header />
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/counter" component={CounterPage} />
-                <Route path="/fetch-data" component={FetchDataPage} />
-                <Route path="/auth" component={AuthPage} />
-            </Switch>
-        </BrowserRouter>
-    </>
-);
-
-export default Root;
+export default class Root extends React.Component<{}> {
+    public render(): JSX.Element {
+        return (
+            <>
+                <Header />
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/" component={HomePage} />
+                        <Route path="/counter" component={CounterPage} />
+                        <Route path="/fetch-data" component={FetchDataPage} />
+                        <Route path="/auth" component={AuthPage} />
+                    </Switch>
+                </BrowserRouter>
+            </>
+        );
+    }
+}
