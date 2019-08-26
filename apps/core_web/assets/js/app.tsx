2dede14b7e8c774ba35edb551ac767e2968cf3e1
doromones <doromones@gmail.com>
2019-08-26 20:02:57 +0300

    connect store

--- apps/core_web/assets/js/app.tsx
+++ apps/core_web/assets/js/app.tsx
@@ -6,6 +6,14 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import Root from './Root'
 
+import {Provider} from 'react-redux';
+import store from "./store";
+
 // This code starts up the React app when it runs in a browser. It sets up the routing
 // configuration and injects the app into a DOM element.
-ReactDOM.render(<Root />, document.getElementById('react-app'))
+ReactDOM.render(
+    <Provider store={store}>
+        <Root/>
+    </Provider>,
+    document.getElementById('react-app')
+)
