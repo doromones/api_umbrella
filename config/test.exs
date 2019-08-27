01a41cb8b8aaf6a50860c65726b40cb759c6c4a3
Anton <doromones@users.noreply.github.com>
2019-08-27 22:12:07 +0300

    Update test.exs

--- config/test.exs
+++ config/test.exs
@@ -12,7 +12,7 @@ config :core, Core.Repo,
   password: "postgres",
   database: "core_test",
   hostname: System.get_env("POSTGRES_HOST", "localhost"),
-  port: System.get_env("POSTGRES_PORT", 5432),
+  port: System.get_env("POSTGRES_PORT", "5432"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
