a160acadc2f7503f72e62aaf45859d24aa60f88f
Anton <doromones@users.noreply.github.com>
2019-08-27 22:57:28 +0300

    Update elixir.yml (#2)
    * Update elixir.yml
    
    * Rename .travis.yml to _.travis.yml
    
    * Update elixir.yml
    
    * Update test.exs
    
    * Update elixir.yml
    
    * Update test.exs
    
    * Update test.exs
    
    * Update elixir.yml
    
    * Update elixir.yml
    
    * Update elixir.yml
    
    * Update elixir.yml
    
    * Update elixir.yml
    
    * Update elixir.yml
    
    * Update elixir.yml
    
    * Update elixir.yml
    
    * Update elixir.yml

--- config/test.exs
+++ config/test.exs
@@ -11,7 +11,8 @@ config :core, Core.Repo,
   username: "postgres",
   password: "postgres",
   database: "core_test",
-  hostname: "localhost",
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
+  port: System.get_env("POSTGRES_PORT", "5432"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
