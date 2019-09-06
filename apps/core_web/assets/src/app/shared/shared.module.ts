c3a0c5f2e74e1636c7188474faee71799f286b60
doromones <doromones@gmail.com>
2019-09-06 22:42:04 +0300

    move components to components

--- apps/core_web/assets/src/app/shared/shared.module.ts
+++ apps/core_web/assets/src/app/shared/shared.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MainComponent } from './main/main.component';
-
-
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, MainComponent],
