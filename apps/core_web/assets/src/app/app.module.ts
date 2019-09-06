b12e7ecbb0c67f0e1c9a9e40eb2d7a29583ed8fd
doromones <doromones@gmail.com>
2019-09-06 23:01:00 +0300

    config

--- apps/core_web/assets/src/app/app.module.ts
+++ apps/core_web/assets/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from './shared/shared.module';
-import {AuthServiceConfig, FacebookLoginProvider, SocialLoginModule} from "angularx-social-login";
+import {AuthServiceConfig, FacebookLoginProvider, SocialLoginModule} from 'angularx-social-login';
 
-let config = new AuthServiceConfig([
+const config = new AuthServiceConfig([
   {
     id: FacebookLoginProvider.PROVIDER_ID,
     provider: new FacebookLoginProvider(window.CONFIG.FACEBOOK_CLIENT_ID)
