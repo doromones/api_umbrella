9842b37d67c9d4c397924b4b5a2272ff7dc6363c
a.radushev <a.radushev@starladder.tv>
2019-10-01 18:56:00 +0300

    ansible

--- ansible/recreate_vagrant.sh
+++ ansible/recreate_vagrant.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+cd "$(dirname "$0")" || exit
+
 vagrant destroy --force
 vagrant up
+
+echo "\n***********"
+echo "Sleep 5 seconds"
+echo "\n***********"
+sleep 5
 
 ansible-playbook 100-vagrant.yml -i inventory/vagrant.yml
